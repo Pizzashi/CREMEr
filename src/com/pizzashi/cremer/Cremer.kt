1b545d3ddab3b68505bc795a9bd4769f01d25e94
Baconfry <baconfry@duck.com>
2023-06-11 02:33:37 +0800

    Exposed some functions

--- src/com/pizzashi/cremer/Cremer.kt
+++ src/com/pizzashi/cremer/Cremer.kt
@@ -10,47 +10,31 @@ class Cremer(container: ComponentContainer) : AndroidNonvisibleComponent(contain
     val math = Math()
     val cvbr = ConstantVolumeBatchReactor()
 
-    @SimpleFunction(description = "Returns the sum of the given list of integers.")
-    fun SumAll(integers: YailList): Int {
-        val integersString = mutableListOf<String>()
-        for (item in integers) {
-            integersString.add(item.toString()) ?: 0
-        }
-
-        return integersString.get(1).toIntOrNull() ?: 0
-        // return integers.sumOf {
-        //   it.toString().toIntOrNull() ?: 0
-        // }
-    }
-
-    @SimpleFunction(description = "LETS FUCKING GOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO")
-    fun HelloWord(): String {
-        val tools = Tools()
-
-        return math.ln(45.45).toString()
-    }
-
-    @SimpleFunction(description = "Test Function")
-    fun TestRegs(inputx: YailList, inputy: YailList): YailList {
-        val tools = Tools()
-        
-        val result = tools.LinearRegression(tools.YailListToDouble(inputx), tools.YailListToDouble(inputy))
-
-        return YailList.makeList(result)
-    }
-
-    @SimpleFunction()
-    fun GetLength(inpuuttt: YailList): Int {
-        return inpuuttt.get(0).toString().toIntOrNull() ?: 0
-    }
-
-
     @SimpleFunction(description = "Solves for 1st order irreversible reaction.")
-    fun FirstOrderIrreversible(x: YailList, y: YailList, type: String): YailList = cvbr.FirstOrderIrreversible(x, y, type)
+    fun FirstOrderIrreversible(time: YailList, a: YailList, type: String): YailList =
+        cvbr.FirstOrderIrreversible(time, a, type)
 
     @SimpleFunction(description = "Solves for 2nd order irreversible reaction in the form of 2A -> R.")
-    fun SecondOrderIrreversible_2A(x: YailList, y: YailList, type: String, Cao: Double): YailList = cvbr.SecondOrderBimolecularIrreversible_2A(x, y, type, Cao)
+    fun SecondOrderIrreversible_2A(time: YailList, a: YailList, type: String, Cao: Double): YailList =
+        cvbr.SecondOrderBimolecularIrreversible_2A(time, a, type, Cao)
 
     @SimpleFunction(description = "Solves for 2nd order irreversible reaction in the form of A + B -> R.")
-    fun SecondOrderIrreversible_AB(x: YailList, y: YailList, type: String, Cao: Double, Cbo: Double): YailList = cvbr.SecondOrderBimolecularIrreversible_AB(x, y, type, Cao, Cbo)
+    fun SecondOrderIrreversible_AB(time: YailList, a: YailList, type: String, Cao: Double, Cbo: Double): YailList =
+        cvbr.SecondOrderBimolecularIrreversible_AB(time, a, type, Cao, Cbo)
+
+    @SimpleFunction(description = "Solves for 3rd order irreversible reaction in the form of A + B + D -> R.")
+    fun ThirdOrderTrimolecularIrreversible_ABD(time: YailList, a: YailList, b: YailList, d: YailList, type: String, Cao: Double, Cbo: Double, Cdo: Double) =
+        cvbr.ThirdOrderTrimolecularIrreversible_ABD(time, a, b, d, type, Cao, Cbo, Cdo)
+
+    @SimpleFunction(description = "Solves for 3rd order irreversible reaction in the form of A + 2B -> R.")
+    fun ThirdOrderTrimolecularIrreversible_A2B(time: YailList, a: YailList, b: YailList, type: String, Cao: Double, Cbo: Double) =
+        cvbr.ThirdOrderTrimolecularIrreversible_A2B(time, a, b, type, Cao, Cbo)
+
+    @SimpleFunction(description = "Solves for 3rd order irreversible reaction in the form of A + B -> R.")
+    fun ThirdOrderTrimolecularIrreversible_AB(time: YailList, a: YailList, b: YailList, type: String, Cao: Double, Cbo: Double) =
+        cvbr.ThirdOrderTrimolecularIrreversible_AB(time, a, b, type, Cao, Cbo)
+
+    @SimpleFunction(description = "Determines the best order of a reaction.")
+    fun NthOrderIrreversible(time: YailList, a: YailList, type: String, Cao: Double) =
+        cvbr.NthOrderIrreversible(time, a, type, Cao)
 }
